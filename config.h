701a716bbe06441a7a0d49c3e4b95dcc974c7f36
Boop <madomilovtimur@gmail.com>
2024-07-31 12:51:26 +0200

    Mas patches

--- config.h
+++ config.h
@@ -10,6 +10,9 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int splitstatus        = 1;        /* 1 for split status items */
+static const char *splitdelim       = ";";      /* Character used for separating statsu */
+
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
 
